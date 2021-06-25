df5566b8910de63eb73bbee3854724e957c0f84c
jrwaheed <j.r.waheed@gmail.com>
2021-06-25 15:22:44 +0200

    last of the car work, delete/insert

---
+++ CarsScript-2.sql
@@ -0,0 +1,7 @@
+insert into cars (`Brand`, `Model`, `Features`, `Year`, `Color`, `SerialNumber`, `Price`, `Sold`) values('Ford','Ranger','Raptor', 2022, 'Midnight','FR343',55000, False)
+
+select * from cars where brand = 'Ford';
+delete from cars where brand ='Ford';
+select * from cars;
+
+delete from cars;
