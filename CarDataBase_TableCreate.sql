85f49c5d44b06f229601f29d7c6440a307da4025
jrwaheed <j.r.waheed@gmail.com>
2021-07-08 19:17:43 +0200

    java to mysql

---
+++ CarDataBase_TableCreate.sql
@@ -0,0 +1,13 @@
+create table cars (
+id int auto_increment primary key,
+model varchar(50),
+price int
+)
+insert into cars (model, price) values ('golf', 17000);
+insert into cars (model, price) values ('polo', 9000);
+insert into cars (model, price) values ('passat', 17000);
+insert into cars (model, price) values ('tiguan', 17000);
+insert into cars (model, price) values ('polo', 19000
+)
+
+
--- Warehouse_Working_Script.sql
+++ Warehouse_Working_Script.sql
@@ -14,3 +14,9 @@ where id= 11;
 update warehouse
 set RackNumber = 17
 where BuyingPrice > 50;
+
+update warehouse set RackNumber = 4 where SellingPrice < 30;
+
+update warehouse set Amount = 200 where ArticleName = 'Canon Druckerpatronen';
+
+update warehouse set Amount = 80 where id = 5;
